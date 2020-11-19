a5375cbf97b3ecb5c227b0630c612b3eb898b0b9
sireliah <sajuukk@gmail.com>
2020-11-19 20:11:55 +0100

    Fix the overlay in the gnome 3.38.

--- extension.js
+++ extension.js
@@ -5,6 +5,7 @@ let on_window_created;
 
 const ExtensionUtils = imports.misc.extensionUtils;
 const Gio = imports.gi.Gio;
+const GObject = imports.gi.GObject;
 const Me = ExtensionUtils.getCurrentExtension();
 const Workspace = imports.ui.workspace;
 const St = imports.gi.St;
@@ -13,10 +14,52 @@ const Markers = Me.imports.markers;
 
 
 let windowConfigs;
-let originalOverlay;
+let originalWorkspace;
 let settings;
 let wmHandler;
 let settingsHandler;
+
+var MyWorkspace = GObject.registerClass(
+    class MyWorkspace extends Workspace.Workspace {
+        _addWindowClone(metaWindow) {
+            const clone = super._addWindowClone(metaWindow);
+
+            // This function is called on the window clone and
+            // attaches marker to the window
+            (function _extendWindow() {
+                const windowName = metaWindow.toString();
+
+                if (windowName in windowConfigs) {
+                    const { uniqueName, color } = windowConfigs[windowName];
+                } else {
+                    setMarker(windowName);
+                }
+
+                this._marker = {};
+                this._marker.box = new St.Bin();
+
+                const { uniqueSymbol, color } = windowConfigs[windowName];
+                this._marker.box.style = `background-color: ${color};
+                                          border-radius: 0 0 10px 0;
+                                          padding: 2px;
+                                          text-align: center;`;
+                this._marker.box.height = 60;
+                this._marker.box.width = 60;
+
+
+                this._marker.label = new St.Label({
+                    style_class: 'extension-distinctWindows-label',
+                    text: `${uniqueSymbol}`
+                });
+                this._marker.box.add_actor(this._marker.label);
+
+                this.add_child(this._marker.box);
+            }).call(clone);
+
+            return clone;
+        }
+    });
+
 
 function resetWindowMarkers() {
     // Replace markers in all existing windowConfigs
@@ -53,6 +96,8 @@ function enable() {
 
     windowConfigs = {};
 
+    Workspace.Workspace = MyWorkspace;
+
     let wm = global.window_manager;
     wmHandler = wm.connect('destroy', function(_shellwm, actor) {
         // Remove configs for windows that has been closed
@@ -65,87 +110,16 @@ function enable() {
         resetWindowMarkers();
     });
 
-    Workspace.WindowOverlay = class extends Workspace.WindowOverlay {
-        constructor(windowClone, parentActor) {
-            super(windowClone, parentActor);
-
-            const meta = this._windowClone.metaWindow;
-            const windowName = meta.toString();
-
-            if (windowName in windowConfigs) {
-                const { uniqueName, color } = windowConfigs[windowName];
-            } else {
-                setMarker(windowName);
-            }
-
-            this._marker = {};
-            this._marker.box = new St.Bin();
-
-            const { uniqueSymbol, color } = windowConfigs[windowName];
-            this._marker.box.style = `background-color: ${color};
-                                      border-radius: 0 0 10px 0;
-                                      padding: 2px;
-                                      text-align: center;`;
-            this._marker.box.height = 60;
-            this._marker.box.width = 60;
-
-
-            this._marker.label = new St.Label({
-                style_class: 'extension-distinctWindows-label',
-                text: `${uniqueSymbol}`
-            });
-            this._marker.box.add_actor(this._marker.label);
-
-            parentActor.add_actor(this._marker.box);
-            parentActor.set_child_below_sibling(this.title, this._marker.box);
-        }
-
-        show() {
-            super.show(...arguments);
-            if (this._marker && this._marker.box) {
-                this._marker.box.show();
-            }
-        }
-
-        hide() {
-            super.hide(...arguments);
-
-            if (this._marker && this._marker.box) {
-                this._marker.box.hide();
-            }
-        }
-
-        relayout(animate) {
-            super.relayout(animate);
-
-            if (this._marker && this._marker.box) {
-                let [x, y, width, height] = this._windowClone.slot;
-                this._marker.box.set_position(x, y);
-            }
-        }
-
-        _onDestroy(animate) {
-            super._onDestroy(animate);
-            const windowActor = this._windowClone.get_label_actor();
-
-            if (this._marker && this._marker.box) {
-                this._marker.box.destroy();
-                if (windowActor) {
-                    delete windowConfigs[windowActor.text];
-                }
-            }
-        }
-    };
 }
 
 function init() {
     log(`Initializing ${Me.metadata.name} version ${Me.metadata.version}`);
-    originalOverlay = Workspace.WindowOverlay;
+    originalWorkspace = Workspace.Workspace;
 }
 
 function disable() {
     log(`Disabling ${Me.metadata.name} version ${Me.metadata.version}`);
-    Workspace.WindowOverlay = originalOverlay;
+    Workspace.Workspace = originalWorkspace;
     windowConfigs = {};
     if (settings && settingsHandler) {
         settings.disconnect(settingsHandler);
